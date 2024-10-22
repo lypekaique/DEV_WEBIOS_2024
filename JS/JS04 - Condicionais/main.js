558d137450e3fcf4cf5b73c62f3cc580c779c67a
lypekaique <lypekaique@hotmail.com>
2024-10-21 21:42:17 -0300

    new file:   JS/JS03/JS04/index.html 	new file:   JS/JS04 - Condicionais/index.html 	new file:   JS/JS04 - Condicionais/main.js 	deleted:    JS/JS04/index.html

---
+++ JS/JS04 - Condicionais/main.js
@@ -0,0 +1,29 @@
+//verificação
+
+
+// let caio = false
+
+// if(caio == true) {
+
+//     console.log('atumaleico')
+// }
+// else{
+//     console.log('VAZA DAKI MEU')
+// }
+
+
+// let pontos = 500
+
+// if(pontos >= 1000){
+//     console.log('ele passou nos pontos')
+// }
+
+
+
+let imparPar = 16
+
+if(imparPar % 2 == 0){
+    console.log('é par')
+}else{
+    console.log('impar')
+}

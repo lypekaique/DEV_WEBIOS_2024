9c564331d52e4cc96daa997ca8cea128860376e4
lypekaique <lypekaique@hotmail.com>
2024-11-04 21:33:50 -0300

    modified:   JS/Emerson/JS05/index.html 	new file:   JS/JS08 - Funcao/engine.js 	new file:   JS/JS08 - Funcao/index.html

---
+++ JS/JS08 - Funcao/engine.js
@@ -0,0 +1,42 @@
+// function contador(){
+
+// let imparpar = 18
+// let numero = 1
+
+
+//     if(imparpar % 2 == 0){
+//         console.log('é par')
+//         for(numero; numero <=50; numero++){
+//             console.log(numero)
+//         }
+//     }else{
+//         console.log('impar')
+//         for(numero; numero <=50; numero++){
+//             console.log(numero)
+//         }
+//     }
+// }
+
+// let boa = () =>{
+//     return 'welcome To the jungle'
+// }
+
+// console.log(boa())
+// let soma = (num1, num2) => {
+//     return num1 + num2;
+
+    
+// }
+// console.log(soma(20,70))
+
+
+// function click(){
+//     let  cliques = 0
+//     return cliques = cliques + 1;
+
+// }
+// console.log(click())
+
+let chamapopup= () =>{
+    alert('parabens.')
+}
